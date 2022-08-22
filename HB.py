35d2fba5749a6e38d58d476881cb7f1c1ffdb036
Tamir Rozenfeld <108065131+TamirRozenfeld@users.noreply.github.com>
2022-08-22 13:01:48 +0300

    Add files via upload

---
+++ HB.py
@@ -0,0 +1,11 @@
+
+from HB_LifeCycle import HoneyBee
+Adult_male = HoneyBee("light brown","meadows", 21, "Tamir")
+Queen = HoneyBee("light brown","meadows", 16, "Yael")
+Pupa = HoneyBee("light brown","meadows", 8, "Edo")
+Larva = HoneyBee("light brown","meadows", 4, "Eran")
+Egg = HoneyBee("light brown","meadows", 1, "David")
+
+
+
+
